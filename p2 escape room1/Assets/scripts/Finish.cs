f9fc4da12e089ccb2cf286f86876a9d018d4d19f
HafflePaffle <Hafsteinn.pe@gmail.com>
2023-05-01 17:30:14 +0200

    Update Finish.cs

--- p2 escape room1/Assets/scripts/Finish.cs
+++ p2 escape room1/Assets/scripts/Finish.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    
     {
-        
+        GameObject puzzle = GameObject.FindGameObjectWithTag("puzzle0");
+        GameObject puzzle1 = GameObject.FindGameObjectWithTag("puzzle1");
+        GameObject puzzle2 = GameObject.FindGameObjectWithTag("puzzle2");
+        if (puzzle == null && puzzle1 == null && puzzle2 == null && SceneManager.GetActiveScene().name == "BlueRoom")
+        {
+            
+        }
     }
 }
