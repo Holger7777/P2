93ef7db9c41ce908645e3802d1de450b8f7e98c1
Holger <115777904+Holger7777@users.noreply.github.com>
2023-05-08 14:04:49 +0200

    000

---
+++ p2 escape room1/Assets/scripts/AnswerChecker.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AnswerChecker : MonoBehaviour
+{
+    public string correctAnswer = "5"; // replace with the correct answer
+
+    private TMP_InputField inputField;
+
+    private void Awake()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputField.onEndEdit.AddListener(CheckAnswer);
+    }
+
+    private void CheckAnswer(string answer)
+    {
+        if (answer == correctAnswer)
+        {
+            Debug.Log("Correct!");
+            // perform some action to indicate success
+        }
+        else
+        {
+            Debug.Log("Incorrect!");
+            // perform some action to indicate failure
+        }
+
+        inputField.text = ""; // clear the input field
+    }
+}
+
